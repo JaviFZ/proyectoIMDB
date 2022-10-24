ed545d53a4d5e2928e5c7ea9ce66fcecd574843c
JaviFZ <jferzap@gmail.com>
2022-10-24 17:22:09 +0200

    actualizo la rama remota con la local

--- professionalTest.ts
+++ professionalTest.ts
@@ -1,7 +1,7 @@
 import {Professional} from "./professional";
 
 
-let professional1 = new Professional("Owen Wilson", 53, 80, 180, false, "estadounidense", 1, "actor");
+export let professional1 = new Professional("Owen Wilson", 53, 80, 180, false, "estadounidense", 1, "actor");
 let professional2 = new Professional("Cate Blanchett", 53, 70, 174, false, "australiana", 2, "actor");
 let professional3 = new Professional("Bill Murray", 72, 87, 188, false, "estadounidense", 0, "actor");
 let professional4 = new Professional("Wes Anderson", 53, 75, 180, false, "estadounidense", 0, "director");
