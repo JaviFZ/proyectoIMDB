c071766cf41f498df0706e1e056fc9093a7682a8
JaviFZ <jferzap@gmail.com>
2022-10-25 17:52:52 +0200

    Subida de proyecto hasta punto7

--- imdb.ts
+++ imdb.ts
@@ -1,7 +1,7 @@
 import {Movie} from "./Movie"
 
 
-
+const fs = require("fs")
 
 
 
@@ -11,4 +11,17 @@ export class IMDB {
     constructor(peliculas : Movie[]){
         this.peliculas = peliculas;
     }
+
+    // // METODOS
+
+    public escribirEnFicheroJSON(nuevaEntrada : string) {    
+        let stringnuevaEntrada = JSON.stringify(this);
+        fs.writeFileSync(nuevaEntrada, stringnuevaEntrada)
+    }
+
+    public obtenerInstancialIMDB(nombreFichero : string) : IMDB{
+        let newObjet = JSON.parse(fs.readFileSync(nombreFichero)); // // JSON.parse nos lo cambia a objeto
+        return newObjet;
+    }
+    
 } 
